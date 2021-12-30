01ae8baf28810bf69fd281da3f65345a40f958c9
Taras <tarashevchuk@gmail.com>
2021-12-30 22:33:54 +0200

    changed package.json

--- index.js
+++ index.js
@@ -1,109 +1,99 @@
-// const express = require('express');
-// const app = express();
-// const url =
-//   'mongodb+srv://taras:werty123@cluster0.4uh3g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'; // your MongoDB url
-// const mongoose = require('mongoose');
-
-// // requests setting
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-//   );
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'X-Requested-With,content-type'
-//   );
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
-
-// mongoose
-//   .connect(url, {
-//     useNewUrlParser: true
-//   })
-//   .then(() => console.log('MongoDb connected...'))
-//   .catch((e) => console.log(e));
-
-// const port = process.env.port || 3000;
-
-// app.listen(port, () => {
-//   console.log('Server started...');
-// });
-
-// const RecordSchema = new mongoose.Schema({
-//   id: {
-//     type: String,
-//     required: true
-//   },
-//   nickname: {
-//     type: String,
-//     required: true
-//   },
-//   numberOfCard: {
-//     type: Number,
-//     required: true
-//   },
-//   cardType: {
-//     type: String,
-//     required: true
-//   },
-//   steps: {
-//     type: Number,
-//     required: false
-//   },
-//   time: {
-//     type: Number,
-//     required: false
-//   },
-//   date: {
-//     type: Date,
-//     required: false
-//   }
-// });
-// const Users = mongoose.model('record', RecordSchema);
-
-// // get request
-// app.get('/records', (req, res) => {
-//   Users.find((err, records) => {
-//     res.json(records);
-//   });
-//   console.log('Get Records');
-// });
-
-// // parse data from post request
-// app.use(express.json());
-// // post request
-// app.post('/records', ({ body }, res) => {
-//   const id = idGenerator();
-
-//   const newRecord = {
-//     ...body,
-//     id
-//   };
-
-//   Users.create(newRecord)
-//     .then(() => {
-//       res.json(newRecord);
-//     })
-//     .catch(() => {
-//       res.status(400).json({ error: 'Data type is wrong' });
-//     });
-
-//   console.log('Add record');
-// });
-
-// function idGenerator() {
-//   return `-${Math.random().toString(16).slice(2)}`;
-// }
 const express = require('express');
 const app = express();
+const url =
+  'mongodb+srv://taras:werty123@cluster0.4uh3g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'; // your MongoDB url
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the home page baby');
+// requests setting
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
 });
 
-app.listen(3000, () => {
-  console.log('Wazzapppp');
+mongoose
+  .connect(url, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('MongoDb connected...'))
+  .catch((e) => console.log(e));
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+  console.log('Server started...');
 });
+
+const RecordSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true
+  },
+  nickname: {
+    type: String,
+    required: true
+  },
+  numberOfCard: {
+    type: Number,
+    required: true
+  },
+  cardType: {
+    type: String,
+    required: true
+  },
+  steps: {
+    type: Number,
+    required: false
+  },
+  time: {
+    type: Number,
+    required: false
+  },
+  date: {
+    type: Date,
+    required: false
+  }
+});
+const Users = mongoose.model('record', RecordSchema);
+
+// get request
+app.get('/records', (req, res) => {
+  Users.find((err, records) => {
+    res.json(records);
+  });
+  console.log('Get Records');
+});
+
+// parse data from post request
+app.use(express.json());
+// post request
+app.post('/records', ({ body }, res) => {
+  const id = idGenerator();
+
+  const newRecord = {
+    ...body,
+    id
+  };
+
+  Users.create(newRecord)
+    .then(() => {
+      res.json(newRecord);
+    })
+    .catch(() => {
+      res.status(400).json({ error: 'Data type is wrong' });
+    });
+
+  console.log('Add record');
+});
+
+function idGenerator() {
+  return `-${Math.random().toString(16).slice(2)}`;
+}
